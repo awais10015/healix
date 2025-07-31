c9af2c3406a2bb9c22a2fedd33a4a62627771d13
awais10015 <awais10015@gmail.com>
2025-07-31 14:43:03 +0500

    hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,9 +90,9 @@ const Navbar = () => {
         </SignedIn> */}
         <header className="flex justify-end items-center p-4 gap-4 h-16">
           <SignedOut>
-            <SignInButton />
+            <SignInButton className="hover:cursor-pointer" />
             <SignUpButton>
-              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              <button className="bg-[#6c47ff] hover:cursor-pointer text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 Sign Up
               </button>
             </SignUpButton>
